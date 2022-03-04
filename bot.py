650c8dcd864cc947c542c05bc2730a9ebeb72edc
ShonTitor <14-10576@usb.ve>
2022-03-03 22:49:03 -0400

    twitter

--- bot.py
+++ bot.py
@@ -4,10 +4,18 @@ import traceback
 from database import *
 from apis import *
 from datetime import datetime
+import twitter
 
 with open(os.path.join(path, "config.json"), "r") as f:
     config = json.loads(f.read())
     db_file = config["db_file"]
+
+# Cosas de twitter
+twitter_api = twitter.Twitter(auth=twitter.OAuth(
+                                config["twitter_access_token_key"], 
+                                config["twitter_access_token_secret"],
+                                config["twitter_consumer_key"],
+                                config["twitter_consumer_secret"]))
 
 conn = create_connection(db_file)
 
@@ -52,28 +60,38 @@ while True :
                 
                 if data["game"] in ["ssbu", "melee"] \
                    and len(data["players"]) >= 8 \
-                   and not all(p["char"][0] == "Random" for p in data["players"]):
+                   and not all(p["char"][0] == "Random" for p in data["players"]\
+                       ):
+
                     top8er = get_top8er(data)
                     if "error" in top8er:
                         printl(f"top8er API error: {top8er['error']}")
                         img = None
                     else:
-                        base64_img = base64.b64decode(top8er["base64_img"])
-                        buffer = io.BytesIO(base64_img)
-                        img = Image.open(buffer)
-                        img.save("result.png")
+                        #base64_img = base64.b64decode(top8er["base64_img"])
+                        #buffer = io.BytesIO(base64_img)
+                        #img = Image.open(buffer)
+                        #img.save("result.png")
+                        img = top8er["base64_img"]
                 else:
                     printl(f"Skipping top 8 graphic for game: {data['game']}")
                     img = None
 
-                if img:
-                    img.show()
-                    input()
-                        
                 result_text = format_data(data)
+                try:
+                    params = {"status": result_text}
+                    if img:
+                        params["media[]"] = img
+                        params["_base64"] = True
+                    twitter_api.statuses.update_with_media(**params)
+                except Exception:
+                    printl(traceback.format_exc())
+                        
                 complete_event(conn, slug)
                     
                 printl(result_text + "\n\n\n")
+                time.sleep(1)
+
     except Exception:
         printl(traceback.format_exc())
 
